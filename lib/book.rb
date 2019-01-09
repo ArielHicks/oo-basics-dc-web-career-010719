e32087b5ed48a89e17ad85eba35773ff96df9472
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-09 22:12:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,14 @@
 
-attr_accessor :author, :page_count, :genre
-attr_reader :title
 
 
   class Book
+attr_accessor :author, :page_count, :genre
   def initialize(title)
       @title = title
   end
+
+  attr_reader :title
+
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
